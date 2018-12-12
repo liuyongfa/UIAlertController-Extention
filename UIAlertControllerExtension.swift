15e5b47a0741a08fb2b27b52f94f759eb05db496
yongfaliu <yongfaliu@yongfadeMacBook-Pro.local>
2018-12-12 16:44:49 +0800

    swift

--- UIAlertControllerExtension.swift
+++ UIAlertControllerExtension.swift
@@ -24,17 +24,16 @@ extension UIAlertController {
     class func showWithTitle(_ title: String, message: String? = nil, buttonTitle: String? = LYFButtonTitle, block: LYFAlertBlcok? = nil) {
         UIAlertController.showWithTitle(title, message: message, otherButtonTitles: [buttonTitle ?? LYFButtonTitle], actionBlocks: block)
     }
-
-    /**
-     显示UIAlertController
-     
-     @param title title
-     @param message message
-     @param rootViewController 在哪个UIViewcontroller上显示，可以为nil
-     @param cancelButtonTitle 取消按钮文字,为nil则没有取消按钮
-     @param otherButtonTitles 其他按钮文字
-     @param actionBlocks 响应按钮的block，如果cancelString不为nil，则第一个为cancel按钮的，之后的block依次对应otherTitles的元素
-     */
+    
+    /// 显示UIAlertController
+    ///
+    /// - Parameters:
+    ///   - title: title
+    ///   - message: message
+    ///   - rootViewController: 在哪个UIViewcontroller上显示，可以为nil
+    ///   - cancelButtonTitle: 取消按钮文字,为nil则没有取消按钮
+    ///   - otherButtonTitles: 其他按钮文字
+    ///   - actionBlocks: 响应按钮的block，如果cancelString不为nil，则第一个为cancel按钮的，之后的block依次对应otherTitles的元素
     class func showWithTitle(_ title: String, message: String? = nil, rootViewController: UIViewController? = nil, cancelButtonTitle: String? = nil, otherButtonTitles: [String], actionBlocks: LYFAlertBlcok?...) {
         let alert = UIAlertController(title: title, message: message ?? "", preferredStyle: .alert)
         var i = 0
